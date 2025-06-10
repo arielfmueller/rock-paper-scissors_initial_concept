19427fc7b72c800cd2d142a0d5500ef3f0db3377
arielfmueller <156081823+arielfmueller@users.noreply.github.com>
2025-06-10 16:29:27 -0300

    Added code for the prompt to get the user's choice.
    Code is still not comparing both choices.

--- java.js
+++ java.js
@@ -1,14 +1,33 @@
-
-
 // Function to get the computer's choice
+
+
+
 function getComputerChoice() {
-  if (Math.random() < 1 / 3) {
-    return "rock";
-  } else if (Math.random() < 2 / 3) {
-    return "paper";
-  } else {
-    return "scissors";
-  }
+    const randomChoice = Math.random()
+    if (randomChoice < 0.34) {
+        return "rock";
+     } else if (randomChoice < 0.67) {
+        return "paper";
+     } else {
+        return "scissors";
+    }
 }
 
-console.log(getComputerChoice());
+// Function to get the human's choice
+function getHumanChoice() {
+    let choice = prompt("What's your choice?", "rock, paper, or scissors");
+    if (choice === null) {
+        return "No choice made";
+    }
+    choice = choice.toLowerCase();
+    if (choice !== "rock" && choice !== "paper" && choice !== "scissors") {
+        return "Invalid choice";
+    }
+    return choice; 
+}
+
+const computerChoice = getComputerChoice() 
+const humanChoice = getHumanChoice()
+
+console.log(computerChoice)
+console.log(humanChoice)
